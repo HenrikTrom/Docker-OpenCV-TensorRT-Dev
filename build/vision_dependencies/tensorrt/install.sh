865d719846ae9b8b7a8e8ba30e1fcbf3eec9c2df
Henrik <training.deepl@gmail.com>
2025-08-05 16:51:14 +0200

    UPDATE: Submodles

--- build/vision_dependencies/tensorrt/install.sh
+++ build/vision_dependencies/tensorrt/install.sh
@@ -1,2 +1,3 @@
 # TensorRT installation script
-tar -xzvf TensorRT-8.6.1.6.Linux.x86_64-gnu.cuda-11.8.tar.gz
+# tar -xzvf TensorRT-8.6.1.6.Linux.x86_64-gnu.cuda-11.8.tar.gz # (old)
+tar -xzvf TensorRT-10.9.0.34.Linux.x86_64-gnu.cuda-12.8.tar.gz # (new)
